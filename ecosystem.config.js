9e4a757cad2a027489d49496a1fe6f99110530d5
Haris <hasanmirza15@gmail.com>
2026-05-31 20:30:10 -0600

    Enable Postgres in production PM2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,14 @@ const productionEnv = {
   PUBLIC_BASE_URL: "https://api.pixelmaniagame.com",
   PUBLIC_WS_URL: "wss://api.pixelmaniagame.com/ws",
   PIXELMANIA_DATA_DIR: "/var/lib/pixelmania",
+  POSTGRES_ENABLED: "true",
+  POSTGRES_HOST: "127.0.0.1",
+  POSTGRES_PORT: "5432",
+  POSTGRES_DATABASE: "pixelmania",
+  POSTGRES_USER: "pixelmania",
+  POSTGRES_PASSWORD: "CHANGE_THIS_PASSWORD",
+  POSTGRES_SSL: "false",
+  POSTGRES_SCHEMA: "pixelmania",
 };
 
 module.exports = {
